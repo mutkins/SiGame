adc601c1caa684146cf775866a72954016d14d23
Mikhail Utkin <mihut@rambler.ru>
2024-02-28 11:27:18 +0500

    работает

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
-from tools import get_question_by_id, configure_questions, mark_question_as_answered, add_score, configure_players, is_there_questions_in_round, mark_round_as_started, get_round_by_num, get_current_round_num
-from flask import Flask, render_template, request
+from tools import get_question_by_id, configure_questions, mark_question_as_answered, add_score, configure_players, is_there_questions_in_round, get_round_by_num
+from flask import Flask, render_template, request, redirect, url_for
 from Players import Players
 
 app = Flask(__name__)
@@ -36,18 +36,15 @@ def render_theme_list(round_num=None):
 def start_round():
     global pack
     round_num = request.args.get('round_num').replace('/', '')
-    pack = mark_round_as_started(pack=pack, round_num=round_num)
-    return render_round_table(round_num=round_num)
+    # return render_round_table(round_num=round_num)
+    return redirect(url_for('render_round_table', round_num=round_num))
 
 
 @app.route('/round_table', methods=['GET'])
 def render_round_table(round_num=None):
     global pack
     if not round_num:
-        if not request.args.get('round_num'):
-            round_num = get_current_round_num(pack=pack)
-        else:
-            round_num = request.args.get('round_num').replace('/', '')
+        round_num = request.args.get('round_num').replace('/', '')
     round = get_round_by_num(pack=pack, round_num=round_num)
     match  round.get("type"):
         case "regular": return render_template('regular_round.html', round=round, players=players)
@@ -79,74 +76,25 @@ def post_answered():
             players = add_score(players=players, player_id=item, score=score)
     if answered_question_id:
         pack = mark_question_as_answered(pack=pack, question_id=answered_question_id)
+    return what_to_do_after_question_answered(pack=pack, round_num=round_num)
+
+
+def what_to_do_after_question_answered(pack, round_num):
     if is_there_questions_in_round(pack=pack, round_num=round_num):
-        return render_round_table(round_num=round_num)
+        # return render_round_table(round_num=round_num)
+        return redirect(url_for('render_round_table', round_num=round_num))
+    elif get_round_by_num(pack=pack, round_num=int(round_num)+1):
+        # return render_theme_list(round_num=int(round_num)+1)
+        return redirect(url_for('render_theme_list', round_num=int(round_num)+1))
     else:
-        return render_theme_list(round_num=int(round_num)+1)
+        return redirect(url_for('render_summary'))
 
 
 
-
-# @app.route('/', methods=['GET'])
-# def render_round_table():
-#     global pack
-#     result = get_current_round_table(pack=pack)
-#     match  result.get("type"):
-#         case "themes_list": return render_theme_list(result.get("round"))
-#         case "round_table": return render_template('regular_round.html', round=result.get("round"), players=players)
-#         case "intuition": return render_template('intuition.html', round=result.get("round"), players=players)
-
-
-
-
-#
-# @app.route('/questions', methods=['GET'])
-# def render_theme_list():
-#     round_num = request.args.get('round_num').replace('/', '')
-#     return render_template('themes_list.html', round=round)
-
-# @app.route('/', methods=['POST'])
-# def post_answered():
-#     global pack
-#     global players
-#     a = request
-#     question_id = None
-#     for item in request.form:
-#         if item.startswith('id'):
-#             question_id = request.form.get(item).replace('/', '')
-#         if item.startswith('player'):
-#             score = request.form.get(item).replace('/', '')
-#             players = add_score(players=players, player_id=item, score=score)
-#     if question_id:
-#         pack = mark_question_as_answered(pack=pack, question_id=question_id)
-#     return render_round_table()
-
-
-# @app.route('/start_round', methods=['POST'])
-# def start_round():
-#     global pack
-#     round_num = request.form.get('round_num').replace('/', '')
-#     pack = mark_round_as_started(pack=pack, round_num=round_num)
-#     a = pack
-#     return render_round_table()
-
-
-# @app.route('/q', methods=['GET'])
-# def show_question():
-#     question_id = request.args.get('id').replace('/', '')
-#     question = get_question_by_id(pack=pack, question_id=question_id)
-#     if question.get("type") == "intuition":
-#         return render_template('intuition_question.html', question=question, players=players)
-#     else:
-#         return render_template('question.html', question=question, players=players)
-
-
-# @app.route('/q', methods=['POST'])
-# def show_question2():
-#     question_id = request.form.get('id').replace('/', '')
-#     question = get_question_by_id(pack=pack, question_id=question_id)
-#     return render_template('question.html', question=question, players=players)
-
+@app.route('/summary', methods=['GET'])
+def render_summary():
+    global players
+    return render_template('summary.html', players=players)
 
 if __name__ == '__main__':
     app.run()
--- tools.py
+++ tools.py
@@ -10,12 +10,12 @@ def get_question_by_id(pack, question_id):
 
 
 def configure_questions(pack):
-    for round in pack:
+    for index, round in enumerate(pack, start=1):
+        round["round_num"] = index
         for theme in round.get("themes"):
             for question in theme.get("questions"):
                 question["id"] = "question" + str(random.randint(0, 1000000))
                 question["answered"] = False
-        round["state"] = None
     return pack
 
 
@@ -25,48 +25,7 @@ def mark_question_as_answered(pack, question_id):
             for question in theme.get("questions"):
                 if question.get("id") == question_id:
                     question["answered"] = True
-
-                    pack = mark_round_as_finished(pack=pack)
                     return pack
-
-
-def mark_round_as_started(pack, round_num):
-    for round in pack:
-        if round.get("round_num") == int(round_num):
-            round["state"] = "in_progress"
-    return pack
-
-
-def mark_round_as_finished(pack):
-    for round in pack:
-        for theme in round.get("themes"):
-            for question in theme.get("questions"):
-                if not question["answered"]:
-                    return pack
-        round["state"] = "finished"
-    return pack
-
-
-def get_current_round_table(pack):
-    for round in pack:
-        if round["state"] is None:
-            return {"type": "themes_list", "round": round}
-        elif round["state"] == "in_progress":
-            return {"type": "regular", "round": round}
-        else:
-            continue
-    return None
-
-
-def get_current_round_num(pack):
-    for round in pack:
-        if round["state"] is None:
-            return round.get("round_num")
-        elif round["state"] == "in_progress":
-            return round.get("round_num")
-        else:
-            continue
-    return None
 
 
 def add_score(players, player_id, score):
